993a446ea655c8f912ceb95e9481c97be6f2bb27
VolkerQuillfeldt <61466301+VolkerQuillfeldt@users.noreply.github.com>
2021-02-10 23:18:36 +0100

    SPring 2.4.2

--- src/main/java/lib/vqui/de/controller/GuestBookController.java
+++ src/main/java/lib/vqui/de/controller/GuestBookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lib.vqui.de.model.dto.GuestBookEntryDto;
 import lib.vqui.de.model.dto.ReturnDto;
 import lib.vqui.de.service.MongoDatabaseService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Slf4j
 public class GuestBookController {
 
 	@Autowired
